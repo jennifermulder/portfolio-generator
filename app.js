667ae7521a2c8355e74735d2f6c5abea5afd04bb
J Mulder <jlmulder2014@gmail.com>
2020-07-16 17:36:26 -0700

    dynamic generate

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
 // inquirer
 //     .prompt([
@@ -45,7 +45,7 @@ const promptUser = () => {
             name: 'confirmAbout',
             message: 'Would you like to enter some information about yourself for an "About" section?',
             default: true
-          },
+        },
         {
             type: 'input',
             name: 'about',
@@ -127,6 +127,7 @@ const promptProject = (portfolioData) => {
             default: false
         }
     ])
+    //portfolioData = promise object from inquirer prompts
         .then(projectData => {
             portfolioData.projects.push(projectData);
             if (projectData.confirmAddProject) {
@@ -137,9 +138,66 @@ const promptProject = (portfolioData) => {
         });
 };
 
+const mockData = 
+    {
+        name: 'Lernantino',
+        github: 'lernantino',
+        confirmAbout: true,
+        about:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+        projects: [
+          {
+            name: 'Run Buddy',
+            description:
+              'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['HTML', 'CSS'],
+            link: 'https://github.com/lernantino/run-buddy',
+            feature: true,
+            confirmAddProject: true
+          },
+          {
+            name: 'Taskinator',
+            description:
+              'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['JavaScript', 'HTML', 'CSS'],
+            link: 'https://github.com/lernantino/taskinator',
+            feature: true,
+            confirmAddProject: true
+          },
+          {
+            name: 'Taskmaster Pro',
+            description:
+              'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+            link: 'https://github.com/lernantino/taskmaster-pro',
+            feature: false,
+            confirmAddProject: true
+          },
+          {
+            name: 'Robot Gladiators',
+            description:
+              'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+            languages: ['JavaScript'],
+            link: 'https://github.com/lernantino/robot-gladiators',
+            feature: false,
+            confirmAddProject: false
+          }
+        ]
+      };
+
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
+        //passing data from command line into "generatePage"
+        const pageHTML = generatePage(portfolioData);
+
+// const pageHTML = generatePage(mockData);
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+
+          console.log('Page created! Check out index.html in this directory to see it!');
+        });
         console.log(portfolioData);
     });
 
